5ab0a720dd32df46d2150e1d5fc4626902885d29
Khaled <khaledouroakpo@gmail.com>
2023-02-06 01:44:32 +0100

    Suite de la configuration de filamentphp et installation du package barryvdh/laravel-debugbar

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,11 @@
 namespace App\Providers;
 
 use Filament\Facades\Filament;
+use Filament\Navigation\NavigationBuilder;
+use Filament\Navigation\NavigationGroup;
+use Filament\Navigation\NavigationItem;
+use Filament\Navigation\UserMenuItem;
+use Filament\Pages\Dashboard;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,10 +27,49 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(): void
     {
+        /**
+         * Configuration générale de filament pour l'application
+         */
         Filament::serving(function () {
+            // Enregistrement du thème de l'application CABALOU
             Filament::registerViteTheme('resources/css/filament.css');
+
+            // Ajout des menus au niveau du dropdown de l'avatar de l'utilisateur
+            Filament::registerUserMenuItems([
+                UserMenuItem::make()
+                    ->label(__('filament::pages/profile.title'))
+                    ->url(route('filament.pages.dashboard'))
+                    ->icon('heroicon-s-user-circle'),
+
+                UserMenuItem::make()
+                    ->label(__('filament::pages/settings.title'))
+                    ->url(route('filament.pages.dashboard'))
+                    ->icon('heroicon-s-cog'),
+            ]);
         });
+
+
+        /**
+         * Organisation des menus en groupes.
+         * Chaque menu appartient à un groupe excepté le menu 'Tableau de bord'
+         */
+        Filament::navigation(function (NavigationBuilder $builder): NavigationBuilder {
+            return $builder
+                ->items([
+                    NavigationItem::make(__('filament::pages/dashboard.title'))
+                        ->icon('heroicon-o-home')
+                        ->isActiveWhen(fn (): bool => request()->routeIs('filament.pages.dashboard'))
+                        ->url(route('filament.pages.dashboard')),
+                ])
+                ->groups([
+                    NavigationGroup::make('Website')
+                        ->items([
+
+                        ]),
+                ]);
+        });
+
     }
 }
